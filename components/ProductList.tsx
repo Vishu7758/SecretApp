127c92a59fdcc9ea89b9ba27fa99d3df10417641
Vishal Kharade <vishalkharade@icloud.com>
2023-06-10 23:41:15 +0530

    Implemented JSON server

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,52 +1,42 @@
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Product, {ProductProps} from './Product';
+import axios from 'axios';
 
 export default function ProductList() {
-  const contacts = [
+  const url = 'http://localhost:3004';
+
+  const [products, setProducts] = useState<
     {
-      uid: 1,
-      name: 'Hitesh Choudhary',
-      status: 'Just an extra ordinary teacher',
-      imageUrl: 'https://avatars.githubusercontent.com/u/11613311?v=4',
-    },
-    {
-      uid: 2,
-      name: 'Anurag Tiwari',
-      status: 'I ❤️ To Code and Teach!',
-      imageUrl: 'https://avatars.githubusercontent.com/u/94738352?v=4',
-    },
-    {
-      uid: 3,
-      name: 'Sanket Singh',
-      status: 'Making your GPay smooth',
-      imageUrl: 'https://avatars.githubusercontent.com/u/29747452?v=4',
-    },
-    {
-      uid: 4,
-      name: 'Anirudh Jwala',
-      status: 'Building secure Digital banks',
-      imageUrl: 'https://avatars.githubusercontent.com/u/25549847?v=4',
-    },
-  ];
+      uid: number;
+      name: string;
+      status: string;
+      imageUrl: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    axios.get(url + '/products').then(response => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.headingText}>ContactList</Text>
-      <ScrollView style={styles.container} scrollEnabled={true}>
-        {contacts.map(({uid, name, status, imageUrl}) => (
-          <View key={uid} style={styles.userCard}>
-            <Image
-              source={{
-                uri: imageUrl,
-              }}
-              style={styles.userImage}
+      <FlatList
+        data={products}
+        renderItem={({item}) => {
+          return (
+            <Product
+              name={item.name}
+              status={item.status}
+              imageUrl={item.imageUrl}
             />
-            <View>
-              <Text style={styles.userName}>{name}</Text>
-              <Text style={styles.userStatus}>{status}</Text>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
+          );
+        }}
+        keyExtractor={item => item.uid.toString()}
+      />
     </View>
   );
 }
